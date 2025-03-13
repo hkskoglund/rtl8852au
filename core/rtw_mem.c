9689638af8303a574ec285ee76c64078393254ea
Henning Skoglund <hkskoglund@gmail.com>
2025-03-13 08:13:03 +0100

    linux kernel 6.13 patch https://bbs.archlinux.org/viewtopic.php?pid=2224262#p2224262

--- core/rtw_mem.c
+++ core/rtw_mem.c
@@ -124,7 +124,11 @@ static void __exit rtw_mem_exit(void)
 }
 
 #if defined(MODULE_IMPORT_NS)
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6, 13, 0))
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#else
+MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
+#endif
 #endif
 
 module_init(rtw_mem_init);
